428a316571e6a6b80004eb5814022e2f6c292d0a
Jodi Henderson <jodi@crannmuir.co.uk>
2017-06-17 22:42:08 +0100

    Fix Custom Size, Add flags and no zeros

--- Minesweeper.java
+++ Minesweeper.java
@@ -4,33 +4,33 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
-
+import java.util.Calendar;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 /**
  * @author Jodi Henderson
  *
  */
 
-public class Minesweeper extends JFrame implements ActionListener{
+public class Minesweeper extends JFrame implements ActionListener, MouseListener{
 
-	private static final int numMines = 10;
-	private static final int row = 10;
-	private static final int column = 10;
+	private static int numMines,row,column,numFlags; 
 	private static final int MINE = 10;
 
 	int[][] mines;
 
 	private JPanel panel;
 	private JButton[][] button;
-	private JLabel time;
-	private JLabel flags;
+	private JLabel time,flags;
 	private JButton newGame;
 	private Container grid;	
 
@@ -38,12 +38,19 @@ public class Minesweeper extends JFrame implements ActionListener{
 
 		setTitle("Minesweeper");
 		setSize(600,400);
+
+		numMines = Integer.parseInt(JOptionPane.showInputDialog("How many mines would you like? "));
+		row = Integer.parseInt(JOptionPane.showInputDialog("How many rows would you like? "));
+		column = Integer.parseInt(JOptionPane.showInputDialog("How many columns would you like? "));
 		
-		//numMines = Integer.parseInt(JOptionPane.showInputDialog("How many mines would you like? "));
-		//row = Integer.parseInt(JOptionPane.showInputDialog("How many rows would you like? "));
-		//column = Integer.parseInt(JOptionPane.showInputDialog("How many columns would you like? "));
 
-
+		while(numMines>(row*column)){
+			JOptionPane.showMessageDialog(this, "More mines than buttons please enter again");
+			numMines = Integer.parseInt(JOptionPane.showInputDialog("How many mines would you like? "));
+			row = Integer.parseInt(JOptionPane.showInputDialog("How many rows would you like? "));
+			column = Integer.parseInt(JOptionPane.showInputDialog("How many columns would you like? "));	
+		}
+		numFlags = numMines;
 		
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(1,3));
@@ -57,7 +64,7 @@ public class Minesweeper extends JFrame implements ActionListener{
 		time.setBackground(Color.LIGHT_GRAY);
 		time.setVisible(true);
 
-		flags = new JLabel("Flags",SwingConstants.CENTER);
+		flags = new JLabel(numFlags+"",SwingConstants.CENTER);
 		flags.setBackground(Color.LIGHT_GRAY);
 		flags.setVisible(true);
 
@@ -76,6 +83,7 @@ public class Minesweeper extends JFrame implements ActionListener{
 			for(int j = 0; j<column; j++){
 				button[i][j]= new JButton();
 				button[i][j].addActionListener(this);
+				button[i][j].addMouseListener(this);
 				grid.add(button[i][j]);
 				button[i][j].setVisible(true);
 			}		
@@ -83,6 +91,7 @@ public class Minesweeper extends JFrame implements ActionListener{
 		add(panel,BorderLayout.NORTH);
 		add(grid);
 		createMines();
+
 		setVisible(true);
 		validate();
 	}
@@ -90,24 +99,29 @@ public class Minesweeper extends JFrame implements ActionListener{
 	 * What happens when a button is clicked
 	 */
 	public void actionPerformed(ActionEvent e) {
+
 		if (e.getSource().equals(newGame)){
 			for(int x = 0; x<row;x++)
 				for(int y =0 ; y<column;y++){
 					button[x][y].setEnabled(true);
 					button[x][y].setText("");
 				}
+			numFlags = numMines;
+			flags.setText(numFlags+"");
 			createMines();
-
 		}else{
 			for(int x = 0; x<row;x++)
 				for(int y =0 ; y<column;y++)
+
 					if (e.getSource().equals(button[x][y]))
 						if (mines[x][y]== MINE){
 							button[x][y].setText("X");
 							showMines();
 						}else if (mines[x][y] == 0){
 							ArrayList<Integer> needsCleared = new ArrayList<Integer>();
-							button[x][y].setText(mines[x][y]+"");
+							if(mines[x][y]!=0)
+								button[x][y].setText(mines[x][y]+"");
+
 							button[x][y].setEnabled(false);
 							needsCleared.add((x*100)+y);
 							clearZeros(needsCleared);
@@ -119,12 +133,29 @@ public class Minesweeper extends JFrame implements ActionListener{
 							checkWin();
 						}
 		}
+		allZeros();
+
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO When i left click after * vanishes
+		if (SwingUtilities.isRightMouseButton(e))
+			for(int x = 0; x<row;x++)
+				for(int y =0 ; y<column;y++)
+					if(e.getSource().equals(button[x][y])){
+						button[x][y].setText("*");
+						numFlags--;
+						flags.setText(numFlags+"");
+					}
+	}     
+
+
 	/**
 	 * Checks surrounding buttons and if they are also a zero it clears them
 	 * @param needsCleared
 	 */
-	public void clearZeros(ArrayList<Integer> needsCleared){
+	private void clearZeros(ArrayList<Integer> needsCleared){
 		if(needsCleared.size() == 0)
 			return;
 		else{
@@ -207,7 +238,7 @@ public class Minesweeper extends JFrame implements ActionListener{
 	/**
 	 * Shows mine or number of neighbour mines
 	 */
-	private void showMines() {
+	private void showMines(){
 		for(int x = 0; x<row; x++)
 			for(int y =0 ; y<column; y++)
 				if(button[x][y].isEnabled())
@@ -218,14 +249,15 @@ public class Minesweeper extends JFrame implements ActionListener{
 						button[x][y].setText("X");
 						button[x][y].setEnabled(false);
 					}	
-		JOptionPane.showMessageDialog(this, "You Lost. Better Luck Next Time");
+		allZeros();
+		JOptionPane.showMessageDialog(this, "You Lose. Better Luck Next Time");
 	}
 
 
 	/**
 	 * Randomly creates and positions mines in the grid
 	 */
-	public void createMines(){
+	private void createMines(){
 		//Initialise list of random pairs
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int x = 0; x<row; x++)
@@ -274,23 +306,68 @@ public class Minesweeper extends JFrame implements ActionListener{
 					mines[x][y] = neighbourMines;
 				}
 	}
-	
-	public void checkWin(){
+
+	private void checkWin(){
 		boolean win = true;
 		for(int x = 0; x<row; x++)
 			for(int y =0 ; y<column; y++)
 				if(mines[x][y] != MINE && button[x][y].isEnabled())
 					win = false;
-		if(win)
+		if(win){
+			allZeros();
 			JOptionPane.showMessageDialog(this, "Congratulations! You've Won!");
-		
-		 
-		
-		
+		}
 	}
+
+	private void allZeros(){
+		for(int x = 0; x<row; x++)
+			for(int y =0 ; y<column; y++)
+				if(mines[x][y] == 0)
+					button[x][y].setText("");
+
+	}
+	/*
+	private void pause(long millisecs){  
+		long current = Calendar.getInstance().getTimeInMillis();
+	    while(Calendar.getInstance().getTimeInMillis()-current<millisecs);
+	}
+
+	//TODO runs forever
+	private void timer(){
+		int seconds = 0;
+		while(true){  	
+			time.setText(seconds+"");
+			pause(1000);
+			seconds++;
+		}
+	}
+	 */
+
 
 	public static void main(String[] args) {
 		new Minesweeper();
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
